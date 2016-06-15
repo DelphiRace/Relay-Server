460a8a9f8352da6afdc8169c914544887e777461
DelphiRace <delphi.race@gmail.com>
2016-06-16 03:17:48 +0800

    update

--- router/Controller/adminRegisteredAPI.php
+++ router/Controller/adminRegisteredAPI.php
@@ -57,9 +57,12 @@ class adminRegisteredAPIController
 
                     if($SysClass->Execute($strSQL)){
                         // $newID = $SysClass->NewInsertID();
+                        $info = array();
+                        $info["uid"] = $customersID;
+                        $info["admin"] = $user_ac;
 
                         $action["msg"] = "註冊成功";
-                        $action["data"] = $customersID;
+                        $action["data"] = $info;
                         $action["status"] = true;
                     }else{
                         $action["msg"] = "管理員帳號註冊失敗";
@@ -68,6 +71,40 @@ class adminRegisteredAPIController
                 }else{
                     $action["msg"] = "相關參數未帶入值";
                 }
+            }
+
+            $pageContent = $SysClass->Data2Json($action);
+        }catch(Exception $error){
+            //依據Controller, Action補上對應位置, $error->getMessage()為固定部份
+            $SysClass->WriteLog("SupplyController", "editorAction", $error->getMessage());
+        }
+        //關閉資料庫連線
+        $SysClass->DBClose();
+        //釋放
+        $SysClass = null;
+        $this->viewContnet['pageContent'] = $pageContent;
+    }
+
+    // 取得管理員帳號列表
+    public function adminlistAction()
+    {
+        $SysClass = new ctrlSystem;
+        // 預設不連資料庫
+        // $SysClass->initialization();
+        // 連線指定資料庫
+        // $SysClass->initialization("設定檔[名稱]",true); -> 即可連資料庫
+        // 連線預設資料庫
+        $SysClass->initialization("cm_auth",true);
+        try{
+            $action = array();
+            $action["status"] = false;
+            
+            $strSQL = "select user_ac,sys_code_uid from ac_admin ";
+            $strSQL .= "where sys_code_uid > 0 ";
+            $data = $SysClass->QueryData($strSQL);
+            if(!empty($data)){
+                $action["status"] = true;
+                $action["data"] = $data;
             }
 
             $pageContent = $SysClass->Data2Json($action);
