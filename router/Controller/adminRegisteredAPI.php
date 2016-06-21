0ac290c9a4ac53b3d70b680a3539eb7cfa1c6103
DelphiRace <delphi.race@gmail.com>
2016-06-21 18:04:21 +0800

    update

--- router/Controller/adminRegisteredAPI.php
+++ router/Controller/adminRegisteredAPI.php
@@ -85,6 +85,60 @@ class adminRegisteredAPIController
         $this->viewContnet['pageContent'] = $pageContent;
     }
 
+    // match管理員帳號
+    public function matchAction()
+    {
+        $SysClass = new ctrlSystem;
+        // 預設不連資料庫
+        // $SysClass->initialization();
+        // 連線指定資料庫
+        // $SysClass->initialization("設定檔[名稱]",true); -> 即可連資料庫
+        // 連線預設資料庫
+        $SysClass->initialization("cm_auth",true);
+        try{
+            $action = array();
+            $action["status"] = false;
+            if(!empty($_POST)){
+                $admin_uuid = $_POST["admin_uuid"];
+                $bps_user_uid = $_POST["bps_user_uid"];
+
+                if($admin_uuid and $bps_user_uid){
+                    $strSQL = "select * from sys_admin_data where uuid = '".$admin_uuid."'";
+                    $data = $SysClass->QueryData($strSQL);
+                    if(empty($data)){
+
+                        $strSQL = "insert into sys_admin_data(uuid,bps_user_uid) ";
+                        $strSQL .= "values('".$admin_uuid."','".$bps_user_uid."');";
+
+                        if($SysClass->Execute($strSQL)){
+                            $action["msg"] = "管理員帳號資訊更新成功";
+                            $action["status"] = true;
+                        }else{
+                            $action["msg"] = "管理員帳號資訊更新失敗";
+                            // $action["msg"] = $strSQL;
+
+                        }
+                    }else{
+                        $action["msg"] = "管理員帳號資訊已有";
+
+                    }
+                }else{
+                    $action["msg"] = "相關參數未帶入值";
+                }
+            }
+
+            $pageContent = $SysClass->Data2Json($action);
+        }catch(Exception $error){
+            //依據Controller, Action補上對應位置, $error->getMessage()為固定部份
+            $SysClass->WriteLog("SupplyController", "editorAction", $error->getMessage());
+        }
+        //關閉資料庫連線
+        $SysClass->DBClose();
+        //釋放
+        $SysClass = null;
+        $this->viewContnet['pageContent'] = $pageContent;
+    }
+
     // 取得管理員帳號列表
     public function adminlistAction()
     {
